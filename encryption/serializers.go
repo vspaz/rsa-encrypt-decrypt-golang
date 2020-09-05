6870cf5a1645c8933fc2c0f732b9505de0ff3f44
vspaz <vladimir.edelman@gmail.com>
2020-09-06 00:17:57 +0200

    renaming

--- encryption/serializers.go
+++ encryption/serializers.go
@@ -24,22 +24,22 @@ func New(block *pem.Block, cryptoHash hash.Hash) *Encryptor {
 
 func (enc *Encryptor) EncryptWithPublicKey(text string) []byte {
 	randomReader := rand.Reader
-	encodedPin := []byte(text)
+	encodedText := []byte(text)
 	var rsaPublicKey *rsa.PublicKey
 	pubInterface, parseErr := x509.ParsePKIXPublicKey(enc.PublicKeyBlock.Bytes)
 	if parseErr != nil {
 		log.Fatal("Failed to load public key")
 	}
 	rsaPublicKey = pubInterface.(*rsa.PublicKey)
-	encryptedPin, encryptErr := rsa.EncryptOAEP(enc.Hash, randomReader, rsaPublicKey, encodedPin, nil)
+	encryptedText, encryptErr := rsa.EncryptOAEP(enc.Hash, randomReader, rsaPublicKey, encodedText, nil)
 	if encryptErr != nil {
 		log.Fatal("Failed to encrypt text with public key")
 	}
-	return encryptedPin
+	return encryptedText
 }
 
-func (enc *Encryptor) ToBase85(pin []byte) string {
-	dest := make([]byte, ascii85.MaxEncodedLen(len(pin)))
-	ascii85.Encode(dest, pin)
+func (enc *Encryptor) ToBase85(text []byte) string {
+	dest := make([]byte, ascii85.MaxEncodedLen(len(text)))
+	ascii85.Encode(dest, text)
 	return string(dest)
 }
