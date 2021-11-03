d51d8e39bf8f29275e4bf4bbbe9ae695a5666d19
vspaz <vladimir.edelman@gmail.com>
2021-11-03 19:57:30 +0100

    extend Decoder

--- pkg/cryptolib/serializers.go
+++ pkg/cryptolib/serializers.go
@@ -71,29 +71,40 @@ func (crypto CryptoObject) FromBase85(text string) []byte {
 	return bytes.Trim(decodedText, "\x00")
 }
 
-type EncodeDecode struct {
-	Hash   hash.Hash
-	Random io.Reader
-}
-
 type Encoder struct {
 	PublicKeyBlock *pem.Block
-	Random          io.Reader
 }
 
 type Decoder struct {
 	PrivateKeyBlock *pem.Block
-	EncodeDecode
 }
 
-func NewDecoder(privateKeyBlock *pem.Block, cryptoHash hash.Hash) *Decoder {
+func NewDecoder(privateKey string) *Decoder {
+	privateKeyBlock, _ := pem.Decode([]byte(privateKey))
 	return &Decoder{
 		PrivateKeyBlock: privateKeyBlock,
-		EncodeDecode: EncodeDecode{
-			cryptoHash,
-			rand.Reader,
-		},
 	}
+}
+
+func (d *Decoder) DecryptWithPrivateKey(text []byte) string {
+	var rsaPrivateKey *rsa.PrivateKey
+	rsaPrivateKey, err := x509.ParsePKCS1PrivateKey(d.PrivateKeyBlock.Bytes)
+	if err != nil {
+		log.Fatal("Failed to load private key")
+	}
+	decryptedText, err := rsa.DecryptOAEP(sha1.New(), rand.Reader, rsaPrivateKey, text, nil)
+	if err != nil {
+		log.Fatal("Failed to decrypt")
+	}
+	return string(decryptedText)
+}
+
+func (d Decoder) FromBase85(text string) []byte {
+	decodedText := make([]byte, len([]byte(text)))
+	decoded, _, _ := ascii85.Decode(decodedText, []byte(text), true)
+	decodedText = decodedText[:decoded]
+	//remove /x00 null bytes appended by ascii85
+	return bytes.Trim(decodedText, "\x00")
 }
 
 func NewEncoder(publicKey string) *Encoder {
